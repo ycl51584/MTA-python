0ef5c7d44748d7a2a6e81c9da8e44d32fcc3d007
ycl51584 <77949721+ycl51584@users.noreply.github.com>
2021-01-25 11:55:57 +0800

    1-1

---
+++ day1-1.py
@@ -0,0 +1,2 @@
+a=input("name?")
+print("hello",a)
---
+++ day1-2.py
@@ -0,0 +1,7 @@
+top=input("top")
+below=input("below")
+high=input("high")
+ans=((float(top)+float(below))*float(high)/2)
+print(eval("ans"))
+#if %(ans/1)!=0:
+   # print(float(ans))
